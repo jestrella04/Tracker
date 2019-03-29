3ac1e2448742e1caf08f3b1d4b70b18c96eb165d
Jonathan Estrella <jestrella04@gmail.com>
2019-03-29 15:29:39 +0000

    Updating Webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,4 @@
-/*var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-module.exports = {
-	context: __dirname + '/public',
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader?minimize=true'],
-				})
-			}
-		]
-	},
-	entry: './static/js/webpack.js',
-	output: {
-		path: __dirname + '/public/static/js',
-		filename: 'bundle.min.js'
-	},
-	plugins: [
-		new ExtractTextPlugin({ filename: '../css/bundle.min.css' }),
-		new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', moment: 'moment' }),
-	],
-};*/
-
+const webpack = require('webpack');
 const TerserJSPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
@@ -39,7 +13,10 @@ module.exports = {
   optimization: {
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
   },
-  plugins: [new MiniCssExtractPlugin({filename: "../css/bundle.min.css", chunkFilename: "[id].css"})],
+  plugins: [
+    new MiniCssExtractPlugin({filename: "../css/bundle.min.css", chunkFilename: "[id].css"}),
+    new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery", moment: "moment"})
+  ],
   module: {
     rules: [
       {
