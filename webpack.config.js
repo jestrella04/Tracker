aa2e5b9488fb0fb88f5f8c8244139c63b97e20ef
Jonathan Estrella <jestrella04@gmail.com>
2019-03-29 15:20:25 +0000

    Migrating to Webpack 4 (and updating dependencies)\nCreating build scripts with Yarn\nImproving configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var webpack = require('webpack');
+/*var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
@@ -23,4 +23,29 @@ module.exports = {
 		new ExtractTextPlugin({ filename: '../css/bundle.min.css' }),
 		new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', moment: 'moment' }),
 	],
+};*/
+
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  context: __dirname + "/public",
+  entry: "./static/js/webpack.js",
+  output: {
+    path: __dirname + "/public/static/js",
+    filename: "bundle.min.js"
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [new MiniCssExtractPlugin({filename: "../css/bundle.min.css", chunkFilename: "[id].css"})],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  }
 };
